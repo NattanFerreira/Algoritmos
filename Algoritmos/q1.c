1b3c5c731dc551c78150a189c26c6a516d423a05
Nattan Ferreira Lopes <129337413+NattanFerreira@users.noreply.github.com>
2025-06-15 18:25:26 -0300

    Add files via upload
    Listas realizadas em algoritmos 1

---
+++ Algoritmos/q1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int f;
+    int c;
+    printf("Digite a temperatura F:");
+    scanf("%d",&f);
+    c = 5 * ((f-32) / 9);
+    printf("A temperatura em C é:%d\n", c);
+}
